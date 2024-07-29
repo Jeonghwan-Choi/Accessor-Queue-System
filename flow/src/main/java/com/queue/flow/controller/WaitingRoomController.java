7d47f74d6b1972ec5ececeb55851097daf24824b
jack <yzon007@naver.com>
2024-07-29 15:24:03 +0900

    update redirect_url

--- flow/src/main/java/com/queue/flow/controller/WaitingRoomController.java
+++ flow/src/main/java/com/queue/flow/controller/WaitingRoomController.java
@@ -16,10 +16,33 @@ public class WaitingRoomController {
 
     private final UserQueueService userQueueService;
 
+    /**
+     * @Description
+     * @1. Check if queue and userId are in acceptable state
+     * @2. Register if not allowed
+     * @3. If the rank is not registered, register the rank
+     * @param queue
+     * @param userId
+     * @param redirectUrl
+     * @return html
+     */
     @GetMapping("/waiting-room")
     Mono<Rendering> waitingRoomPage(@RequestParam(name = "queue", defaultValue = "default") String queue,
-                                    @RequestParam(name = "user_id") Long userId) {
-        return Mono.just(Rendering.view("waiting-room.html")
-                .build());
+                                    @RequestParam(name = "user_id") Long userId,
+                                    @RequestParam(name = "redirect_url") String redirectUrl) {
+        // 대기 등록
+        // 웹페이지 필요한 데이터를 전달
+        return userQueueService.isAllowed(queue, userId)
+                .filter(allowed -> allowed)
+                .flatMap(allowed -> Mono.just(Rendering.redirectTo(redirectUrl).build()))
+                .switchIfEmpty(userQueueService.registerWaitQueue(queue, userId)
+                        .onErrorResume(ex -> userQueueService.getRank(queue, userId))
+                        .map(rank -> Rendering.view("waiting-room.html")
+                                .modelAttribute("number", rank)
+                                .modelAttribute("userId", userId)
+                                .modelAttribute("queue", queue)
+                                .build()
+                        ));
+
     }
 }
